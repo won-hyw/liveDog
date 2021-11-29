c661618cbc38d55d91a61947ddaa63ca1861308f
won-hyw <w2019@e-mirim.hs.kr>
2021-11-30 00:35:25 +0900

    플레이어 이동 구현, 플레이어 점프 스프라이트 시트 코드 구현

--- game.py
+++ game.py
@@ -15,26 +15,39 @@ class Game:
 
         self.gameScore = 0
 
+        # 리소스 불러오기
+        # 맵 1
         self.background = pygame.image.load(os.path.join(DIR_IMAGE, 'background_map_1.png'))
-
-        player = AnimatedSprite(position=(150, 400))
-        self.all_sprites = pygame.sprite.Group(player)
+        # 플레이어 이동 애니메이션 클래스
+        self.player = AnimatedSprite(position=(150, 400))
+        self.all_sprites = pygame.sprite.Group(self.player)
 
         self.run()
 
     def run(self):
         while True:
-            # screen.fill((255, 255, 255))
-            self.all_sprites.update()
-            screen.blit(self.background, (0, 0))
-            self.all_sprites.draw(screen)
-            pygame.display.update()
-            clock.tick(8)
+            # 각 loop를 도는 시간
+            # clock.tick()은 밀리초를 반환하므로 1000을 나누어주어 초 단위로 변경
+            mt = clock.tick(60) / 1000
+
             for event in pygame.event.get():  # 어떤 이벤트가 발생하였는가?
                 if event.type == pygame.QUIT:  # 창이 닫히는 이벤트가 발생하였는가?
                     pygame.quit()
                     sys.exit()
-        pygame.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+                        self.player.state = 1
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+                        self.player.state = 0
+
+            # all_sprites 그룹 안에 든 모든 Sprite update
+            self.all_sprites.update(mt)
+            # 배경화면 설정
+            screen.blit(self.background, (0, 0))
+            # 모든 sprite 화면에 그리기
+            self.all_sprites.draw(screen)
+            pygame.display.update()
 
 
 if __name__ == "__main__":
