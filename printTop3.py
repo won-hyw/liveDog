66a4269923e379775eb38cded78dc87aa4a78534
won-hyw <w2019@e-mirim.hs.kr>
2021-12-02 08:56:35 +0900

    점수 페이지 내림차순 정렬 수정

--- printTop3.py
+++ printTop3.py
@@ -25,24 +25,23 @@ class Top3:
                 if line == '':
                     break
                 line_data = line.rstrip().split('\t')
-                count_list.append(line_data)
-        count_list.sort()
+                count_list.append(line_data[1])
+        count_list.sort(reverse=True)
         return count_list[:3]
 
     def run(self, player):
         font = pygame.font.Font('font/DungGeunMo.ttf', 30)
         font_color = pygame.color.Color((255, 255, 255))
-        print(player)
 
-        player_1 = font.render(f'1등  {player[0][0]}님    점수 : {player[0][1]}', True, font_color)
-        player_2 = font.render(f'2등  {player[1][0]}님    점수 : {player[1][1]}', True, font_color)
-        player_3 = font.render(f'3등  {player[2][0]}님    점수 : {player[2][1]}', True, font_color)
+        player_1 = font.render(f'1등  {player[0]}', True, font_color)
+        player_2 = font.render(f'2등  {player[1]}', True, font_color)
+        player_3 = font.render(f'3등  {player[2]}', True, font_color)
 
         while True:
             SCREEN.fill((0, 0, 0))
-            SCREEN.blit(player_1, (280, 280))
-            SCREEN.blit(player_2, (280, 320))
-            SCREEN.blit(player_3, (280, 360))
+            SCREEN.blit(player_1, (380, 280))
+            SCREEN.blit(player_2, (380, 320))
+            SCREEN.blit(player_3, (380, 360))
             pygame.display.update()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -52,6 +51,7 @@ class Top3:
                         esc_alert = pyautogui.confirm(text='정말로 종료하시겠습니까?', title='경고', buttons=['확인', '취소'])
                         if esc_alert == '확인':
                             pygame.quit()
+        pygame.quit()
 
     def run2(self):
         font = pygame.font.Font('font/DungGeunMo.ttf', 30)
